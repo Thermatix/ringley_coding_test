618986d3404fc7bd3be2ccb042c38254b91c0894
Martin Becker <mbeckerwork@gmail.com>
2016-09-23 19:46:05 +0000

    now allows date input when executing script

--- service_charge_reminder.rb
+++ service_charge_reminder.rb
@@ -4,7 +4,7 @@ require 'date'
 
 estates = load_yml('estates.yml')
 reminder_rules = load_yml('rules.yml')
-date = DateTime.now
+date = ARGV.empty? ? DateTime.now : DateTime.new(*ARGV.map(&:to_i))
 
 reminder = Reminder.new(reminder_rules)
 reminder.on(date, estates)
